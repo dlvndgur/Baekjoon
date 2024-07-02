de49c68c040d44d23b541f15e1ab4b624e0ab2e5
JHKIM <poteto5663@gmail.com>
2024-07-02 13:18:51 +0900

    method3 만들었음

--- boj_algo_1.py
+++ boj_algo_1.py
@@ -10,8 +10,15 @@ def method2():
     li = np.array([randint(1,100) for _ in range(randint(1,100))])
     print(li, '\nlength of li : ', len(li))
 
+def method3():
+    for _ in range(10):
+        for i in range(10):
+            print('*' * i)
+
 if __name__ == '__main__':
     method1()
 
     method2()
     print('왜 안되냐고')
+
+    method3()
