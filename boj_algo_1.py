031a90fbd4f25deae909994b1f3b9b70fc650a41
JHKIM <poteto5663@gmail.com>
2024-07-02 13:23:43 +0900

    define method3

--- boj_algo_1.py
+++ boj_algo_1.py
@@ -12,6 +12,12 @@ def method2():
     li = np.array([randint(1,100) for _ in range(randint(1,100))])
     print(li, '\nlength of li : ', len(li))
 
+def method3():
+    s = set()
+    while len(s) <= 20:
+        s.add(randint(1, 100))
+    print(s)
+
 if __name__ == '__main__':
     method1()
 
